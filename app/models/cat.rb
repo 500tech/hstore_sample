55b6bc8492592a9d9c1fd82e49f179dd03d3a5a0
Boris Dinkevich <boris@dinkevich.com>
2014-10-27 19:09:49 +0200

    Add a simple boolean conversion of 'neutered' to Cat

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -2,4 +2,8 @@ class Cat < ActiveRecord::Base
   store_accessor :details, :family, :neutered, :age
 
   validates_presence_of :family
+
+  def neutered
+    super == 'true' or super == true
+  end
 end
